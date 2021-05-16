e7f0e9e7225573f7629afe7ba0dcccdccf24bec3
rogelio1502 <rogelio15021515@gmail.com>
2021-05-16 13:19:12 -0500

    se agrego modelo estanteria y la carpte templates

--- estanteria_website/productos/models.py
+++ estanteria_website/productos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Estanteria(models.Model):
+
+    modelo= models.CharField(verbose_name="Nombre del Modelo",max_length=50,null=False)
+    color = models.CharField(verbose_name="Color",max_length=50,null=False)
+    precio = models.FloatField(verbose_name="Precio",null=False)
+
+    class Meta:
+        verbose_name = "Estantenria"
+        verbose_name_plural = "Estanterias"
+
+    def __str__(self):
+        return self.modelo
+
+    
--- estanteria_website/productos/views.py
+++ estanteria_website/productos/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hols")
+    return render(request,"home.html")
