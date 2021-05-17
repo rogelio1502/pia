589d3f8775aa5293a199fd888adf33d448adfa36
rogelio1502 <rogelio15021515@gmail.com>
2021-05-17 12:40:55 -0500

    se añadieron estilos y grid

--- estanteria_website/productos/urls.py
+++ estanteria_website/productos/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('productos/',views.productos,name="productos"),
+    path('sucursales/',views.sucursales,name="sucursales"),
+    path('quienes_somos/',views.quienes_somos,name="quienes"),
 ]
--- estanteria_website/productos/views.py
+++ estanteria_website/productos/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,"home.html")
+
+
+
+def productos(request):
+    return render(request,"productos.html")
+
+
+
+def sucursales(request):
+    return render(request,"sucursales.html")
+
+
+def quienes_somos(request):
+    return render(request,"quienessomos.html")
